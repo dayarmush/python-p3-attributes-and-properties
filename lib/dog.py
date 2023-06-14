14a36f5ce4431a5dcffa76d101d1f68205abacec
Dovid Yarmush <yarmushda@gmail.com>
2023-06-14 13:18:53 -0400

    complete dog class

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,39 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='dog', breed = None ):
+            if type(name) == str and (0 < len(name) < 26):
+                self.name = name
+            else:
+                return print('Name must be string between 1 and 25 characters.')
+
+            if breed in APPROVED_BREEDS:
+                self.breed = breed
+            else:
+                print("Breed must be in list of approved breeds.")
+
+
+    # def set_name(self, name):
+    #     if type(name) == str and (0 < len(name) < 26):
+    #             self.name = name
+    #     else:
+    #         return print('Name must be string between 1 and 25 characters.')
+
+    # def get_name(self):
+    #     return self._name
+
+    # name = property(set_name, get_name)
+
+    # def set_breed(self, breed):
+    #     if breed in APPROVED_BREEDS:
+    #             self.breed = breed
+    #     else:
+    #         print("Breed must be in list of approved breeds.")
+         
+
+    
+    # def get_breed(self):
+        # return self._breed
+    
+    # breed = property(set_breed, get_breed)
+    
